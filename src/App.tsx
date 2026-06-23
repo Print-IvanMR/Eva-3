c37f4c2117d215688653f9c145e14fb8c82e48e0
Print-IvanMR <ivanmartinezrd14@gmail.com>
2026-06-23 18:29:21 -0400

    Commit React (ivan)
    hice el translado del codigo anterior al formato React para la evaluacion 3

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Login</h1>} />
+      <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+    </Routes>
+  );
+}
+
+export default App;
